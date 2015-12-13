b3f23f599684dfe5e80e7b1c9a27fe31e873eee4
Andrew Bresee <andrew.p.bresee@gmail.com>
2015-12-13 15:27:13 -0800

    Almost done with stringify.
    Still need to have the function not put "," at the end of each object at the end of the string.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var newString = ""
+    	if(obj.constructor === Array){
+    		newString += "["
+    		for(var i = 0; i<obj.length; i++){
+    			newString += string(obj[i])
+    		}
+    		newString += "]"
+    	} else if(obj.constructor === Object){
+    		newString += "{";
+    		for(var key in obj){
+    			newString += key
+    			newString += ":"
+    			newString += string(obj[key])
+    		}
+    		newString += "}"
+    		newString += ","
+    	} else {
+    		var type = (typeof obj);
+    		if(type != "undefined" || type != "boolean" || type != "function" || type != "symbol"){
+    			newString += obj; 
+    			newString += ","; 
+    		} else {
+    			newString += "null"
+    		}
+    		return newString;
+    }
+    return newString; 
 };
+
