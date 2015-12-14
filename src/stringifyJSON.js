5ecdf34adf59dc7dfc4e05b5519e94b5726fb883
Andrew Bresee <andrew.p.bresee@gmail.com>
2015-12-13 16:08:00 -0800

    Nearly done with stringify. Want to still be able to remove last "," from the very last "]"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,22 +4,31 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	var newString = ""
+    	var newString = ""; 
+   
     	if(obj.constructor === Array){
     		newString += "["
     		for(var i = 0; i<obj.length; i++){
     			newString += string(obj[i])
     		}
+
+    		newString = newString.slice(0, -1);
     		newString += "]"
+    		newString += ","
+
     	} else if(obj.constructor === Object){
     		newString += "{";
     		for(var key in obj){
+    			objIndex += 1;
+
     			newString += key
     			newString += ":"
-    			newString += string(obj[key])
+    			newString += string(obj[key]);
     		}
+    		newString = newString.slice(0, -1);
     		newString += "}"
     		newString += ","
+    		
     	} else {
     		var type = (typeof obj);
     		if(type != "undefined" || type != "boolean" || type != "function" || type != "symbol"){
@@ -30,6 +39,8 @@ var stringifyJSON = function(obj) {
     		}
     		return newString;
     }
+};
+
     return newString; 
 };
 
