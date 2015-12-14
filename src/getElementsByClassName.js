da5e3dee023b74ff7724c4a548f1c1d5f0353193
Andrew Bresee <andrew.p.bresee@gmail.com>
2015-12-14 15:08:16 -0800

    Finished up getElementByClassName.
    Want to refactor a bit still.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,33 @@
+
+
 // If life was easy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  
+  var results = [];
+  var currentNode = document.body;
+
+  var childChecker = function(node){
+    console.log("class Names Test: " + node.classList)
+
+    if(node.classList != undefined){
+      if(node.classList.contains(className)){
+        results.push(node)
+      }
+    }
+
+
+    for(var i=0; i<node.childNodes.length ; i++){
+      childChecker(node.childNodes[i])
+    }
+
+  }
+
+  childChecker(currentNode)
+  
+  return results; 
 };
