f8e1c567235f0b9cf49f9414d58e8f0cedf98c88
Andrew Bresee <andrew.p.bresee@gmail.com>
2015-12-13 16:26:49 -0800

    Added comments to better understand control flow.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,32 +9,35 @@ var stringifyJSON = function(obj) {
     	if(obj.constructor === Array){
     		newString += "["
     		for(var i = 0; i<obj.length; i++){
+    			//the + "," adds between each element. [123] --> [1,2,3,]
     			newString += stringifyJSON(obj[i]) + ","; 
     		}
+
+    		//Removes the "," at the end of a list, so [1,2,3,] --> [1,2,3]
     		newString = newString.slice(0, -1);
     		newString += "]"; 
-
     	} else if(obj.constructor === Object){
     		newString += "{";
     		for(var key in obj){
     			newString += key
     			newString += ":"
+    			//the + "," adds between each element. {key:valuekey:vale} --> {key:value, key:vale,}
     			newString += stringifyJSON(obj[key]) + ",";
     		}
+
+    		//Removes the "," at the end of a list, so {1:2, 3:4,} --> {1:2, 3:4}
     		newString = newString.slice(0, -1);
     		newString += "}"; 
-
     	} else {
     		var type = (typeof obj);
     		if(type != "undefined" || type != "boolean" || type != "function" || type != "symbol"){
-
     			newString += obj; 
     		} else {
     			newString += "null"
     		}
     		return newString;
     	}
-    	
+
     return newString; 
 };
 
