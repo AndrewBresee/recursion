f7fd36b746a4e9cb21435b033227b5573c90b54a
Andrew Bresee <andrew.p.bresee@gmail.com>
2015-12-13 21:38:58 -0800

    Did some refactoring on stringify. More still needs to be done.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -26,28 +26,28 @@ var stringifyJSON = function(obj) {
             newString = newString.slice(0, -1);
         }
 		newString += "]"; 
+
 	} else if(obj.constructor === Object){
 		newString += "{";
+
 		for(var key in obj){
 			//the + "," adds between each element. {key:valuekey:vale} --> {key:value, key:vale,}
+            //Probably can refactor this because it happens in the next case as well
             if(typeof obj[key] != 'function' && typeof obj[key] != 'undefined' && obj[key] != "undefined"){
                 newString += "\""+key+"\"";
                 newString += ":"; 
                 newString += stringifyJSON(obj[key]) + ",";
             } 
 		}
-
 		//Removes the "," at the end of a list, so {1:2, 3:4,} --> {1:2, 3:4}
         if(newString != "{"){
             newString = newString.slice(0, -1);
         }
 		newString += "}"; 
 
-
 	} else {
 		var type = (typeof obj);
 		if(type != "undefined" || type != "function" || type != "symbol"|| type != null){
-            console.log("Inside Test: "+obj)
             if(type === 'number' || type === 'boolean'){
                 newString += obj; 
             } else {
