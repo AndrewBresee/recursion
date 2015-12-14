ee55f76d50500a84d4fc600684c0a6ac33104088
Andrew Bresee <andrew.p.bresee@gmail.com>
2015-12-13 20:40:38 -0800

    Stringify was not passing tests. I got it to pass the tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,41 +3,61 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
 	var newString = ""; 
 
-    	if(obj.constructor === Array){
-    		newString += "["
-    		for(var i = 0; i<obj.length; i++){
-    			//the + "," adds between each element. [123] --> [1,2,3,]
-    			newString += stringifyJSON(obj[i]) + ","; 
-    		}
+    if(obj === undefined){ 
+        return newString; 
+    } else if (obj === null){
+        newString += "null";
+        return newString;
+    }
 
-    		//Removes the "," at the end of a list, so [1,2,3,] --> [1,2,3]
-    		newString = newString.slice(0, -1);
-    		newString += "]"; 
-    	} else if(obj.constructor === Object){
-    		newString += "{";
-    		for(var key in obj){
-    			newString += key
-    			newString += ":"
-    			//the + "," adds between each element. {key:valuekey:vale} --> {key:value, key:vale,}
-    			newString += stringifyJSON(obj[key]) + ",";
-    		}
+    if(obj.constructor === Array){
+		newString += "["
+		for(var i = 0; i<obj.length; i++){
+			//the + "," adds between each element. [123] --> [1,2,3,]
+			newString += stringifyJSON(obj[i]) + ","; 
+		}
 
-    		//Removes the "," at the end of a list, so {1:2, 3:4,} --> {1:2, 3:4}
-    		newString = newString.slice(0, -1);
-    		newString += "}"; 
-    	} else {
-    		var type = (typeof obj);
-    		if(type != "undefined" || type != "boolean" || type != "function" || type != "symbol"){
-    			newString += obj; 
-    		} else {
-    			newString += "null"
-    		}
-    		return newString;
-    	}
+		//Removes the "," at the end of a list, so [1,2,3,] --> [1,2,3]
+        if(newString != "["){
+            newString = newString.slice(0, -1);
+        }
+		newString += "]"; 
+	} else if(obj.constructor === Object){
+		newString += "{";
+		for(var key in obj){
+			//the + "," adds between each element. {key:valuekey:vale} --> {key:value, key:vale,}
+            if(typeof obj[key] != 'function' && typeof obj[key] != 'undefined' && obj[key] != "undefined"){
+                newString += "\""+key+"\"";
+                newString += ":"; 
+                newString += stringifyJSON(obj[key]) + ",";
+            } 
+		}
 
+		//Removes the "," at the end of a list, so {1:2, 3:4,} --> {1:2, 3:4}
+        if(newString != "{"){
+            newString = newString.slice(0, -1);
+        }
+		newString += "}"; 
+
+
+	} else {
+		var type = (typeof obj);
+		if(type != "undefined" || type != "function" || type != "symbol"|| type != null){
+            console.log("Inside Test: "+obj)
+            if(type === 'number' || type === 'boolean'){
+                newString += obj; 
+            } else {
+                newString += "\""+obj+"\""; 
+            }
+		} else {
+			newString += "null"
+		}
+		return newString;
+	}
     return newString; 
 };
 
